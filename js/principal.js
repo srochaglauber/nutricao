7fc73e05b04b0a5f11d007a120a05bf41f4c01e0
Glauber Rocha <srochaglauber@gmail.com>
2022-07-03 16:14:43 -0300

    Buscando dados do paciente

--- js/principal.js
+++ js/principal.js
@@ -1,4 +1,12 @@
-console.log("Fui carregado de um arquivo externo.");
-
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-peso");
+var altura = tdAltura.textContent;
+
+var imc = peso / altura * altura;
